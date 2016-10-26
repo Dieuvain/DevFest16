a837aa8dc3997285e37c338277a623aac0f3d130
Michelo <misamuna@gmail.com>
2016-10-26 16:26:14 +0200

    ajout des fragments

---
+++ app/src/main/java/com/example/dieuvainmusaghi/devfest16/fragment/Contact.java
@@ -0,0 +1,30 @@
+package com.example.dieuvainmusaghi.devfest16.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dieuvainmusaghi.devfest16.R;
+
+/**
+ * 
+ * Created by DieuvainMusaghi on 10/22/16.
+ */
+
+public class Contact extends Fragment {
+
+    public Contact(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.location, container, false);
+
+        return rootView;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dieuvainmusaghi/devfest16/fragment/Location.java
@@ -0,0 +1,29 @@
+package com.example.dieuvainmusaghi.devfest16.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.dieuvainmusaghi.devfest16.R;
+
+/**
+ *
+ * Created by DieuvainMusaghi on 10/22/16.
+ */
+
+public class Location extends Fragment {
+
+    public Location(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.location, container, false);
+
+        return rootView;
+    }
+
+
+}
